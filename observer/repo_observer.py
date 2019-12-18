57a13521793b0466462963e24e881fe63e9efe6f
Alex Kiura <kiuraalex@gmail.com>
2019-12-18 07:53:34 +0300

    Poll for repo changes indefinitely

--- observer/repo_observer.py
+++ observer/repo_observer.py
@@ -4,6 +4,8 @@ latest commit id so that the dispatcher can dispatch the tests against
 this commit id
 """
 import argparse
+import helpers
+import subprocess
 
 
 def poll():
@@ -17,3 +19,12 @@ def poll():
                         help="path to the repository to observe")
     args = parser.parse_args()
     dispatcher_host, dispatcher_port = args.dispatcher_server.split(":")
+
+    while True:
+        try:
+            # call the bash script to update the repo and check
+            # for changes.
+            subprocess.check_output(["../scripts/update_repo.sh", args.repo])
+        except subprocess.CalledProcessError as e:
+            raise Exception("Could not update and check repository. " +
+                            "Reason: %s" % e.output)
